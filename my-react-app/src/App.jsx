c0d295234dec146cc55acdc82f71f51d66cf25a6
sindhu sabhavath <sindhuchowhan437@gmail.com>
2024-09-05 14:12:52 +0530

    react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import "./App.css";
+import Axios from 'axios'
+function App()
+{
+const[rollno,setRollno]= useState("");
+const[name,setStudentName]= useState("");
+const[section,setSection]= useState("");
+const[pass,setPromoted]= useState("");
+const[credits,setCredits]= useState("");
+const submitReview=()=>
+{
+Axios.post('http://localhost:9000/aliens',
+{rollno:rollno,
+name:name,
+section:section,
+pass:pass,
+credits:credits}).then(()=>
+{
+alert("success");
+});
+};
+return (
+<div className="App">
+<h1>CRUD Application Demo</h1>
+<div className="information">
+<label><b>rollno</b></label>
+<input
+type="Number"
+rollno="rollno"
+onChange={(e)=>{
+setRollno(e.target.value);
+}}
+required/>
+<label><b>Student name</b></label>
+<input
+type="String"
+name="name"
+onChange={(e)=>{
+setStudentName(e.target.value);
+}}
+required/>
+<label><b>section</b></label>
+<input
+type="string"
+section="section"
+onChange={(e)=>{
+setSection(e.target.value);
+}}
+required/>
+<label><b>pass</b></label>
+<input
+type="Boolean"
+pass="pass"
+onChange={(e)=>{
+setPromoted(e.target.value);
+}}
+required/>
+<label><b>credits</b></label>
+<input
+type="Number"
+credits="credits"
+onChange={(e)=>{
+setCredits(e.target.value);
+}}
+required/>
+<button onClick={submitReview}><b>Submit</b></button>
+</div>
+</div>);
+}
+export default App;
+
--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-//const cors=require('cors')
+const cors=require('cors')
 const modelRouter = require('./routes/aliens')
 
 
 //const url = 'mongodb://localhost:27017/cbit'
-const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+const url = 'mongodb+srv://sindhuchowhan437:160122737158@merncrud.u2q4j.mongodb.net/?retryWrites=true&w=majority&appName=merncrud';
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -15,7 +16,7 @@ con.on('open', () =>
 {
 console.log('connected...')
 })
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use('/aliens',modelRouter)
